09335a34d068f23f601a1f963b3b1f0dd359d8fe
siwhyatt <siwhyatt@gmail.com>
2024-06-07 14:57:35 +0000

    Colour code todos based on priority

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,42 @@
 import './App.css'
-import { Stack, Container } from '@chakra-ui/react'
+import { Stack, Container, Tabs, TabList, Tab, TabPanel, TabPanels } from '@chakra-ui/react'
 import Navbar from './components/Navbar'
 import TodoForm from './components/TodoForm'
+import RegistrationForm from './components/RegistrationForm'
+import LoginForm from './components/LoginForm'
+import { useState } from 'react'
+import TodoList from './components/TodoList'
+
+export const BASE_URL = "http://localhost:5000/api"
 
 function App() {
-
+  const [token, setToken] = useState<string | null>(null);
   return (
     <>
-      <Stack h="100vh">
+      <Stack h="100vh" >
         <Navbar />
         <Container>
-          <TodoForm />
+          {!token ? (
+            <Tabs>
+              <TabList>
+                <Tab>Register</Tab>
+                <Tab>Login</Tab>
+              </TabList>
+              <TabPanels>
+                <TabPanel>
+                  <RegistrationForm />
+                </TabPanel>
+                <TabPanel>
+                  <LoginForm setToken={setToken} />
+                </TabPanel>
+              </TabPanels>
+            </Tabs>
+          ) : (
+            <>
+              <TodoForm token={token} />
+              <TodoList token={token} />
+            </>
+          )}
         </Container>
       </Stack>
     </>
